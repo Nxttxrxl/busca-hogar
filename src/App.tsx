464c17deb4a9592425601e7172b884c991885326
Anouar Kajaj <anouarkajajrosario@gmail.com>
2024-08-08 18:52:04 +0200

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Filtros from './components/Filtros';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import About from './components/About';
-import Resultados from './components/Resultados';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Filtros from "./components/Filtros";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import About from "./components/About";
+import Resultados from "./components/Resultados";
+import Colabora from "./components/Colabora";
 
 const App: React.FC = () => {
-
   return (
     <Router>
       <div className="min-h-screen flex flex-col bg-gray-100 items-center">
@@ -16,24 +16,12 @@ const App: React.FC = () => {
           <Routes>
             <Route path="/about" element={<About />} />
             <Route path="/colabora" element={<Colabora />} />
+            <Route path="/resultados" element={<Resultados />} />
           </Routes>
 
           <main className="flex flex-1 justify-center items-start">
             <Filtros />
           </main>
-    <div className="min-h-screen flex flex-col bg-gray-100 items-center">
-      <div className='w-[1200px] bg-white'>
-
-      <Header />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/resultados" element={<Resultados />} />
-      </Routes>
-      
-      <main className="flex-grow flex-1 justify-center items-start">
-        <Filtros />
-      </main>
-
           <Footer />
         </div>
       </div>
